40d26f01297386a32fe3d78487d9a21612f926da
ROHIT BISHT <bishtrohit590@gmail.com>
2024-06-10 22:43:06 +0530

    main and css for main container done

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,18 +1,61 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import runChat from '../config/gemini.js';
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
 
-    const onSent = async (prompt) =>{
-         
-       await runChat(prompt)
+    const [input , setInput ] = useState("");
+    const [recentPrompt,setRecentPrompt] = useState("");
+    const [prevPrompts , setPrevPrompts]=useState([]);
+    const [showResult, setShowResult] =useState(false);
+    const [loading,setLoading] =useState(false);
+    const [resultData,setResultData]=useState("");
+
+
+    const delayPara = (index,nextWord) => {
+      
+    }
+   
+   
+    const onSent = async (prompt) =>{ 
+
+        setResultData("")
+        setLoading(true)
+        setShowResult(true)
+        setRecentPrompt(input)
+        const response = await runChat(input)
+        let responseArray = response.split("**");
+        let newResponse ;
+        for(let i=0; i<responseArray.length ;i++){
+            if(i=== 0 || i%2 !==1){
+                 newResponse +=responseArray[i];
+            }
+            else{
+                newResponse += "<b>" +responseArray[i]+"</b>";
+            }
+
+        }
+        let newResponse2 = newResponse.split("*").join("</br>")
+        setResultData(newResponse2)
+        setLoading(false)
+        setInput("")
+
     }
 
-    onSent("what is react js")
     
     const contextValue = {
+        prevPrompts,
+        setPrevPrompts,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        loading,
+        resultData,
+        input,
+        setInput,
+        showResult,
+        
 
     } 
     
