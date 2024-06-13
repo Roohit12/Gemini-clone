df8740ec0f6e4d52d876e88562642de2f07c1cb5
ROHIT BISHT <bishtrohit590@gmail.com>
2024-06-13 22:40:21 +0530

    basic UI chnages with responsive

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -14,19 +14,36 @@ const ContextProvider = (props) => {
 
 
     const delayPara = (index,nextWord) => {
-      
+        setTimeout(function(){
+             setResultData(prev => prev+nextWord);
+        },75*index)
     }
-   
-   
+
+    const newChat = () => {
+        setLoading(false)
+        setShowResult(false)
+    }
+
     const onSent = async (prompt) =>{ 
 
         setResultData("")
         setLoading(true)
         setShowResult(true)
-        setRecentPrompt(input)
-        const response = await runChat(input)
+        let response;
+        if(prompt !== undefined){
+            response = await runChat(prompt)
+            setRecentPrompt(prompt)
+
+        }
+         else
+        {
+            setPrevPrompts(prev=>[...prev,input])
+            setRecentPrompt(input)
+            response = await runChat(input)
+        }
+        
         let responseArray = response.split("**");
-        let newResponse ;
+        let newResponse ="" ;
         for(let i=0; i<responseArray.length ;i++){
             if(i=== 0 || i%2 !==1){
                  newResponse +=responseArray[i];
@@ -37,8 +54,12 @@ const ContextProvider = (props) => {
 
         }
         let newResponse2 = newResponse.split("*").join("</br>")
-        setResultData(newResponse2)
-        setLoading(false)
+        let newResponseArray = newResponse2.split(" ");
+         for ( let i=0; i<newResponseArray.length; i++){
+            const nextWord =newResponseArray[i];
+            delayPara(i,nextWord+" ")
+         }
+         setLoading(false)
         setInput("")
 
     }
@@ -55,8 +76,7 @@ const ContextProvider = (props) => {
         input,
         setInput,
         showResult,
-        
-
+        newChat
     } 
     
     return (
